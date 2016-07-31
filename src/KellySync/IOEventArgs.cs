a371f0e1821ee105ad742cf8fc7fc6c3ea17dfc7
Kelly Elton <its.the.doc@gmail.com>
2016-07-31 15:10:12 -0400

    Added a comment

--- src/KellySync/IOEventArgs.cs
+++ src/KellySync/IOEventArgs.cs
@@ -8,16 +8,21 @@ namespace KellySync
 		public WatcherChangeTypes ChangeType { get; }
 		public string FullPath { get; }
 		public string Name { get; }
+        /// <summary>
+        /// If <see cref="FullPath"/> is a directory. The only time this should be null is if 
+        /// the <see cref="ChangeType"/> is <see cref="WatcherChangeTypes.Deleted"/> OR
+        /// if the file/directory got deleted before we could determin what it was.
+        /// </summary>
 		public bool? IsDirectory { get; }
 		public bool IsRename { get; }
 		public string OldFullPath { get; }
 		public string OldName { get; }
 
 		public IOEventArgs( FileSystemEventArgs fileSystemArgs ) {
+			IsDirectory = IsPathDirectory(FullPath);
 			ChangeType = fileSystemArgs.ChangeType;
 			FullPath = fileSystemArgs.FullPath;
 			Name = fileSystemArgs.Name;
-			IsDirectory = IsPathDirectory(FullPath);
 			var rename = fileSystemArgs as RenamedEventArgs;
 			if (rename != null) {
 				IsRename = true;
