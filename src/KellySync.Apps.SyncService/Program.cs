f2756ecbef57b185958380648c5cee13a52fd205
Kelly Elton <its.the.doc@gmail.com>
2022-05-18 14:35:33 -0500

    Switch to .net 6 worker service for SyncService

--- src/KellySync.Apps.SyncService/Program.cs
+++ src/KellySync.Apps.SyncService/Program.cs
@@ -1,41 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
+using KellySync.Apps.SyncService;
 
-namespace KellySync
-{
-    class Program
-    {
-        static log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
 
-        static void Main( string[] args ) {
-            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            Trace.Listeners.Add(new ConsoleTraceListener());
-            Run().Wait();
-        }
-
-        private static void CurrentDomain_UnhandledException( object sender, UnhandledExceptionEventArgs e ) {
-            //throw new NotImplementedException();
-        }
-
-        static async Task Run() {
-            var config = new Config() {
-                RootPath = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\OneDrive\KellySync")
-            };
-            using (var service = new KellySyncService(config)) {
-                service.RegisterFileSyncs(GetFileSyncs(config));
-
-                await Task.WhenAny(Task.Run(() => Console.ReadLine()), service.Run());
-            }
-        }
-
-        static IEnumerable<FileSync> GetFileSyncs( Config config ) {
-            yield return new FileSync(config, @"%USERPROFILE%\SyncTest");
-			yield return new FileSync( config, @"%USERPROFILE%\vimfiles" );
-			yield return new FileSync( config, @"%USERPROFILE%", ".bashrc" );
-			yield return new FileSync( config, @"%USERPROFILE%", ".gitconfig" );
-			yield return new FileSync( config, @"%USERPROFILE%", ".vimrc" );
-		}
-    }
-}
+await host.RunAsync();
